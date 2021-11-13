4f375eddd8286b3fd2c757bb8a6fe18d2c9f4391
ThienNien <tncupi@gmail.com>
2021-11-13 19:51:28 +0700

    newversion

--- LoadDataset.py
+++ LoadDataset.py
@@ -1,15 +1,8 @@
 from angle_data import AngleSteering
-from torch.utils.data import DataLoader
-from torchvision.transforms import transforms
-import torch
-batch_size = 16
 
-dataset = AngleSteering(csv_file="data_angle.csv",root_dir="data",transform=transforms.ToTensor())
+dataset = AngleSteering(csv_file="",root_dir="",transform=)
 
-train_set,test_set = torch.utils.data.random_split(dataset,[1028,400])
+train_set,test_set = torch.utils.data.random_split(dataset,[,])
 
-train_loader = DataLoader(dataset = train_set,batch_size = batch_size, shuffle = True)
-test_loader = DataLoader(dataset = test_set,batch_size = batch_size, shuffle = True)
-
-for i,(data,label) in enumerate(train_loader):
-    pass
+train_loader = Dataloader(dataset = train_set,batch_size = batch_size, shuffle = True)
+test_loader = Dataloader(dataset = test_set,batch_size = batch_size, shuffle = True)
