1307dbf042c5bf943e0f4f07420056e54bd14cc9
ThienNien <tncupi@gmail.com>
2021-11-19 10:09:07 +0700

    densnet

--- LoadDataset.py
+++ LoadDataset.py
@@ -3,17 +3,20 @@ from torch.utils.data import DataLoader
 import torch
 from torchvision import transforms
 batch_size = 16
-transform_re = transforms.Compose([
-        transforms.Resize(32),
-        transforms.ToTensor()
-    ])
+transform_t = transforms.Compose([
+    transforms.RandomCrop(32, padding=4),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
+])
 csv_path =r"C:\Users\tncup\Desktop\Densenet\data_angle.csv"
 data = r"C:\Users\tncup\Desktop\Densenet\data"
-dataset = AngleSteering(csv_file=csv_path,root_dir=data,transform=transform_re)
+dataset = AngleSteering(csv_file=csv_path,root_dir=data,transform=transform_t)
 
 train_set,test_set = torch.utils.data.random_split(dataset,[1028,400])
 
 train_loader = DataLoader(dataset = train_set,batch_size = batch_size, shuffle = True)
 test_loader = DataLoader(dataset = test_set,batch_size = batch_size, shuffle = True)
 
-print(train_set,test_loader)
+classes = ( '-21', '-15', '-5','0', '5', '15', '21')
+
--- test.py
+++
@@ -1,2 +0,0 @@
-import pandas as pd
-df = pd.read_csv(r"C:\Users\tncup\Desktop\Densenet\data_angle.csv")
